cf8705b50b72994604261c631f13889eaa327a92
Tim Jonsson <tim.jonsson@easypark.net>
2023-07-06 08:43:43 +0200

    Returning challenge attribute

--- processSlackBotTaggedMessage/index.js
+++ processSlackBotTaggedMessage/index.js
@@ -1,7 +1,15 @@
 const functions = require('@google-cloud/functions-framework');
 
 functions.http('processSlackBotTaggedMessage', (req, res) => {
-  const payload = req.body;
-  console.log(payload)
-  res.status(200).send('Success');
+  const challenge = req.body.challenge;
+
+  if (challenge) {
+    const response = {
+      challenge: challenge
+    };
+
+    res.status(200).json(response);
+  } else {
+    res.status(400).send('Invalid request');
+  }
 });
