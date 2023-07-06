801bb1f8ee0ed11d5e182ba697efce43572f7a23
Tim Jonsson <tim.jonsson@easypark.net>
2023-07-06 08:21:32 +0200

    Changing entry point name and applying eu region

--- processSlackBotTaggedMessage/index.js
+++ processSlackBotTaggedMessage/index.js
@@ -1,6 +1,6 @@
 const functions = require('@google-cloud/functions-framework');
 
-functions.http('helloHttp', (req, res) => {
+functions.http('processSlackBotTaggedMessage', (req, res) => {
   const payload = req.body;
   console.log(payload)
   res.status(200).send('Success');
